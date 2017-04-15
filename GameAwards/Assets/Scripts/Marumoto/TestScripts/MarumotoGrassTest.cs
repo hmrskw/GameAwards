74b21724850ae4d9a0e16c9da3c4052b9c0cd6ec
KeitaMarumoto <marumoto.keita@gmail.com>
2017-04-15 14:51:41 +0900

    .gitignoreファイルを変更。
    テストケースを上げるためにignoreファイルからTestScriptsの項目を削除しました。
    Grassの動作を簡易的にテストするためのスクリプトもここで上げておきます。

---
+++ GameAwards/Assets/Scripts/Marumoto/TestScripts/MarumotoGrassTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarumotoGrassTest : MonoBehaviour
+{
+	private List<Grass> _grasses;
+
+	void Start ()
+	{
+		_grasses = GetComponentsInChildren<Grass>().ToList();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!Input.GetKeyDown(KeyCode.G)) return;
+
+		foreach(Grass _grass in _grasses)
+		{
+			StartCoroutine(_grass.Growth());
+		}
+	}
+}
