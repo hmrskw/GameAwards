1dd0996e44ea19d3bd8e92e6c33bfbe2f97734b3
KeitaMarumoto <marumoto.keita@gmail.com>
2017-05-15 15:43:24 +0900

    草オブジェクト一つ一つのデータを保持するクラスを作成。
    Object自身とそれにアタッチされているGrassControllerコンポーネントを保持しています。

---
+++ GameAwards/Assets/Scripts/Marumoto/Grass/GrassObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GrassObject {
+	public GameObject Object { get; private set; }
+	public GrassesController Controller { get; private set; }
+
+	public GrassObject(GameObject _obj, GrassesController _ctrl)
+	{
+		Object = _obj;
+		Controller = _ctrl;
+	}
+}
