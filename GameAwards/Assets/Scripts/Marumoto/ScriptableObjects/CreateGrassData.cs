9d98d32ffb56e0661cdca31d9c223504f608ecc9
KeitaMarumoto <marumoto.keita@gmail.com>
2017-05-15 15:41:33 +0900

    草オブジェクト関連のシリアライズしておきたい値をScriptableObjectにしました。

---
+++ GameAwards/Assets/Scripts/Marumoto/ScriptableObjects/CreateGrassData.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CreateGrassData : ScriptableObject
+{
+	[Header("テラインの大きさ")]
+	public int TerrainSize;
+
+	[Space(5)]
+
+	[Header("生成するチャンク数(奇数のみ)")]
+	public int ChunkDepth;
+	public int ChunkWidth;
+
+	[System.NonSerialized, Tooltip("草オブジェクト1つあたりのサイズ")]
+	public int ObjectSize = 4;
+
+	[MenuItem("ScriptableObject/Instantiate GrassDatas")]
+	public static void CreateInstance()
+	{
+		var _asset = CreateInstance<CreateGrassData>();
+
+		AssetDatabase.CreateAsset(_asset, "Assets/Resources/ScriptableObjects/GrassData.asset");
+		AssetDatabase.Refresh();
+	}
+}
