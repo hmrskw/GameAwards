684439b1ace8f6a2248dda29c829fcc54395cbd6
KeitaMarumoto <marumoto.keita@gmail.com>
2017-05-15 17:17:09 +0900

    ScriptableObjectの生成方法を変えました。

---
+++ GameAwards/Assets/Editor/CreateGrassData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CreateGrassData : MonoBehaviour
+{
+	[MenuItem("ScriptableObject/Instantiate GrassDatas")]
+	public static void CreateInstance()
+	{
+		var _asset = ScriptableObject.CreateInstance<GrassData>();
+
+		AssetDatabase.CreateAsset(_asset, "Assets/Resouces/ScriptableObjects/GrassData.asset");
+		AssetDatabase.Refresh();
+	}
+}
---
+++ GameAwards/Assets/Scripts/Marumoto/ScriptableObjects/GrassData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassData : ScriptableObject
+{
+	[Header("テラインの大きさ")]
+	public int TerrainSize;
+
+	[Space(5)]
+
+	[Header("生成するチャンク数(奇数のみ)")]
+	public int ChunkDepth;
+	public int ChunkWidth;
+
+	[System.NonSerialized, Tooltip("草オブジェクト1つあたりのサイズ")]
+	public int ObjectSize = 4;
+}
