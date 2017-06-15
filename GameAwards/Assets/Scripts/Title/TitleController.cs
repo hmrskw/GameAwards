82420a651848dacef156981835202f696c67864d
KeitaMarumoto <marumoto.keita@gmail.com>
2017-06-16 01:49:49 +0900

    タイトルからゲームメインにシーンチェンジできるようにしました。
    1.何かボタンを押すと世界観ポップアップ。
    2.一定時間経過でAボタンが有効になるので、押すとフェードしてからGamemainに飛ぶ。

--- GameAwards/Assets/Scripts/Title/TitleController.cs
+++ GameAwards/Assets/Scripts/Title/TitleController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 /// <summary>
@@ -7,59 +8,78 @@ using UnityEngine.SceneManagement;
 /// BuildSettingsのインデックス１がMainのシーンになるようにお願いします。
 /// </summary>
 public class TitleController : MonoBehaviour {
-	private bool _progressLoading;
-	private bool _canChange;
-	private AsyncOperation _nextSceneOpe;
+	[SerializeField, Tooltip("何かボタンが押されたときに表示する世界観のポップアップ")]
+	GameObject _Popup;
 
-	private void Awake ()
-	{
-		_progressLoading = false;
-		_canChange = false;
-	}
+	[SerializeField, Tooltip("GameMainに移動する時に押すボタンを表示したUI")]
+	GameObject _button;
 
-	void Update ()
+	[SerializeField, Tooltip("タイトルロゴとPushAnyButtonの文字")]
+	GameObject _logos;
+
+	[SerializeField, Tooltip("フェードアウト用のマスク画像")]
+	Image _fadeMaskImage;
+
+	[SerializeField, Tooltip("フェードのアニメーション用")]
+	AnimationCurve _fadeCurve;
+
+	[SerializeField, Tooltip("フェードアウトに掛かる時間(0秒だとゼロ除算でエラー)")]
+	float _fadeTime;
+
+	private bool _hasPushedAnyButtonAtFirst = false;
+	private bool _isEnableGameStartButton = false;
+
+	private void Update ()
 	{
-		if (!_progressLoading)
+		if (!_hasPushedAnyButtonAtFirst)
 		{
 			if (Input.anyKeyDown)
 			{
-				LoadSceneAsyncBackground(1);
+				StartCoroutine(DisplayPopup());
 			}
 		}
 
-		if (_nextSceneOpe != null)
+		if (_isEnableGameStartButton)
 		{
-			if (_nextSceneOpe.isDone)
+			if (Input.GetButtonDown("Submit"))
 			{
-				_nextSceneOpe.allowSceneActivation = false;
+				StartCoroutine(GoToGamemain());
 			}
 		}
-
-		if (_canChange)
-		{
-
-		}
-
-		if (Input.GetKeyDown(KeyCode.G))
-		{
-			MoveNextScene(0);
-		}
 	}
 
-	void LoadSceneAsyncBackground(int _sceneIndex)
+	private IEnumerator DisplayPopup()
 	{
-		_progressLoading = true;
-		_nextSceneOpe = SceneManager.LoadSceneAsync(_sceneIndex, LoadSceneMode.Additive);
-		_nextSceneOpe.allowSceneActivation = false;
+		_logos.SetActive(false);
+		_Popup.SetActive(true);
+
+		yield return new WaitForSeconds(3.0f);
+
+		_isEnableGameStartButton = true;
+		_button.SetActive(true);
 	}
 
-	/// <summary>
-	/// 現在のシーンを無効化し
-	/// ロードしたシーンを有効化
-	/// </summary>
-	public void MoveNextScene(int _sceneIndex)
+	private IEnumerator GoToGamemain()
 	{
-		SceneManager.UnloadSceneAsync(_sceneIndex);
-		_nextSceneOpe.allowSceneActivation = true;
+		yield return StartCoroutine(FadeOutScreen());
+		SceneManager.LoadScene(1);
+	}
+
+	private IEnumerator FadeOutScreen()
+	{
+		Color _color = new Color(0, 0, 0, 0);
+		float _startTime = Time.timeSinceLevelLoad;
+		float _curveValue = 0.0f;
+
+		while (_curveValue < 1.0f)
+		{
+			float _elapsedTime = Time.timeSinceLevelLoad - _startTime;
+			float _elapsedTimeRatio = _elapsedTime / _fadeTime;
+			_curveValue = _fadeCurve.Evaluate(_elapsedTimeRatio);
+
+			_color.a = _curveValue;
+			_fadeMaskImage.color = _color;
+			yield return null;
+		}
 	}
 }
