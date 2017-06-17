3480af431d11bffca739e53c85cb799fbb8b35fe
KeitaMarumoto <marumoto.keita@gmail.com>
2017-06-17 19:19:11 +0900

    シーンフェード用のオブジェクト作成。
    DontDestroyOnLoadです。
    シーン遷移時のフェードでのみ使います。

---
+++ GameAwards/Assets/Resouces/FadeManager/FadeManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeManager : MonoBehaviour {
+	private FadeManager _instance;
+	public FadeManager Instance
+	{
+		get { return _instance; }
+	}
+
+	[SerializeField]
+	Image _fadeImage;
+
+	public bool isFading { get; private set; }
+
+	public IEnumerator FadeScene(int _sceneIndex, float _fadeinTime, float _fadeoutTime)
+	{
+		yield return null;
+	}
+
+	private IEnumerator FadeIn(float _fadeinTime)
+	{
+		yield return null;
+	}
+
+	private IEnumerator FadeOut(float _fadeoutTime)
+	{
+		yield return null;
+	}
+
+	private void Awake()
+	{
+		if(_instance == null)
+		{
+			_instance = this;
+		}
+
+		isFading = false;
+		DontDestroyOnLoad(transform.gameObject);
+	}
+}
