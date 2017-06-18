9b8a7a7fa2fd5617d6ac640f11dc0839c763de0a
KeitaMarumoto <marumoto.keita@gmail.com>
2017-06-18 16:36:26 +0900

    シーン遷移用のフェードマネージャ完成。
    LoadSceneAsyncをつかうことを前提として、UnloadScene機能を内部に持つフェード。
    フェードアウトとインの時間とフェードの色も指定できます。

--- GameAwards/Assets/Resouces/FadeManager/FadeManager.cs
+++ GameAwards/Assets/Resouces/FadeManager/FadeManager.cs
@@ -2,32 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// *************************************************
+/// 制作者 丸本慶大
+/// 2017/06/18
+/// *************************************************
+/// DontDestroyOnLoadでシーン間フェードを管理するクラス。
+/// LoadSceneAsyncを使用することを前提にしています。
+/// *************************************************
+/// </summary>
 public class FadeManager : MonoBehaviour {
-	private FadeManager _instance;
-	public FadeManager Instance
+	private static FadeManager _instance;
+	public static FadeManager Instance
 	{
 		get { return _instance; }
 	}
+	[SerializeField]
+	AnimationCurve _fadeoutCurve;
+
+	[SerializeField]
+	AnimationCurve _fadeinCurve;
 
 	[SerializeField]
 	Image _fadeImage;
 
-	public bool isFading { get; private set; }
+	public bool IsFading { get; private set; }
 
-	public IEnumerator FadeScene(int _sceneIndex, float _fadeinTime, float _fadeoutTime)
+	/// <summary>
+	/// シーンを跨いでコルーチンを動かしたいのでラッピングする。
+	/// 
+	/// 直接コルーチンを呼ぶと、そのシーンが無効になった段階でコルーチンも止まるため、
+	/// コルーチンの呼び出し元を、DontDestroyOnLoadのオブジェクトにする対策。
+	/// </summary>
+	/// <param name="_unloadIndex">Unloadしたいシーンのインデックス</param>
+	/// <param name="_fadeoutTime">フェードアウトにかかる時間(秒)</param>
+	/// <param name="_fadeInTime">フェードインにかかる時間(秒)</param>
+	/// <param name="_maskColor">フェード時のマスク画像の色</param>
+	/// <param name="_nextOpe">次のシーンのAsyncOperation</param>
+	public void FadeScene(int _unloadIndex, float _fadeoutTime, float _fadeInTime, Color _maskColor, AsyncOperation _nextOpe)
 	{
-		yield return null;
+		StartCoroutine(FadeSceneImpl(_unloadIndex, _fadeoutTime, _fadeInTime, _maskColor, _nextOpe));
 	}
 
-	private IEnumerator FadeIn(float _fadeinTime)
+	private IEnumerator FadeSceneImpl(int _unloadSceneIndex, float _fadeoutTime, float _fadeinTime, Color _maskColor, AsyncOperation _nextOpe)
 	{
-		yield return null;
+		if (IsFading) yield break;
+		IsFading = true;
+		_fadeImage.gameObject.SetActive(true);
+
+		yield return StartCoroutine(Fade(_fadeoutTime, _maskColor, _fadeoutCurve));
+
+		if (_nextOpe != null)
+		{
+			if (!_nextOpe.allowSceneActivation)
+			{
+				_nextOpe.allowSceneActivation = true;
+			}
+		}
+		var _unloadOpe = SceneManager.UnloadSceneAsync(_unloadSceneIndex);
+		yield return _unloadOpe;
+		
+		yield return StartCoroutine(Fade(_fadeinTime, _maskColor, _fadeinCurve));
+
+		_fadeImage.gameObject.SetActive(false);
+		IsFading = false;
 	}
 
-	private IEnumerator FadeOut(float _fadeoutTime)
+	private IEnumerator Fade(float _fadeTime, Color _maskColor, AnimationCurve _curve)
 	{
-		yield return null;
+		float _startTime = Time.timeSinceLevelLoad;
+		float _elapsedTimeRatio = 0.0f;
+		_maskColor.a = _curve.Evaluate(0.0f);
+		Color _color = _maskColor;
+
+		while (_elapsedTimeRatio < 1.0f)
+		{
+			float _elapsedTime = Time.timeSinceLevelLoad - _startTime;
+			_elapsedTimeRatio = _elapsedTime / _fadeTime;
+
+			_color.a = _curve.Evaluate(_elapsedTimeRatio);
+			_fadeImage.color = _color;
+
+			yield return null;
+		}
+
+		_color.a = _curve.Evaluate(1.0f);
+		_fadeImage.color = _color;
 	}
 
 	private void Awake()
@@ -37,7 +99,7 @@ public class FadeManager : MonoBehaviour {
 			_instance = this;
 		}
 
-		isFading = false;
+		IsFading = false;
 		DontDestroyOnLoad(transform.gameObject);
 	}
 }
