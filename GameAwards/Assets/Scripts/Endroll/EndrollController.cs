bd9472b943e790af7e84620ace6a32923f95f468
KeitaMarumoto <marumoto.keita@gmail.com>
2017-06-24 17:32:20 +0900

    文字スクロール実装。

---
+++ GameAwards/Assets/Scripts/Endroll/EndrollController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndrollController : MonoBehaviour
+{
+	[SerializeField]
+	RectTransform _rect;
+
+	[SerializeField]
+	float _fadeValue;
+
+	[SerializeField]
+	float _moveTime;
+
+	[SerializeField]
+	AnimationCurve _curve;
+
+	private void Start()
+	{
+		StartCoroutine(ScrollStaffRoll());
+	}
+
+	IEnumerator ScrollStaffRoll()
+	{
+		float _startTime = Time.timeSinceLevelLoad;
+		float _elapsedTimeRatio = 0.0f;
+		Vector3 _position = _rect.localPosition;
+		float _rectY = _rect.position.y;
+
+		while (_elapsedTimeRatio <= 1.0f)
+		{
+			float _elapsedTime = Time.timeSinceLevelLoad;
+			_elapsedTimeRatio = _elapsedTime / _moveTime;
+			float _value = _curve.Evaluate(_elapsedTimeRatio);
+
+			_position.y = _rectY + _fadeValue * _value;
+			_rect.localPosition = _position;
+			yield return null;
+		}
+	}
+}
